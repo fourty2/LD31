b026e3bc981f74828c142184b55d54c1f5c814c4
Marc Henklein <snooze82@gmail.com>
2014-12-06 11:25:24 +0100

    init

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ LD31
 ====
 
 My LudumDare #31 Compo Entry
+
+Entire Game on One Screen
+
---
+++ js/game.js
@@ -0,0 +1,82 @@
+var ld31 = {
+	gamecanvas: null,
+	renderer: null,
+	scene: null,
+	camera: null,
+
+	init: function() {
+		var WIDTH = window.innerWidth;
+		var HEIGHT = window.innerHeight;
+		var NEAR = 0.1;
+		var FAR = 1000;
+		var FOV = 45; // maybe stick with non-perspective mode
+
+		this.gamecanvas = document.getElementById('gamecanvas');
+		this.renderer = new THREE.WebGLRenderer({canvas: gamecanvas});
+ 			this.renderer.setSize( 800, 600 );
+        //document.body.appendChild( this.renderer.domElement );
+		this.renderer.setSize(WIDTH, HEIGHT);
+
+		this.scene = new THREE.Scene();
+
+		// init camera
+		this.camera = new THREE.PerspectiveCamera(FOV, WIDTH/ HEIGHT, NEAR, FAR);
+	    this.camera.position.set( -15, 20, 100 );
+	    this.camera.rotation.z = 180 * Math.PI/3;
+		//this.scene
+		this.initWorld();
+
+
+
+		this.animate();
+	},
+	initWorld: function() {
+
+		
+
+		var sphere = new THREE.SphereGeometry(10, 32, 32);
+		//var plane = new THREE.PlaneGeometry(25, 25, 1,1);
+
+		var mesh =  new THREE.Mesh(sphere,
+						new THREE.MeshLambertMaterial({color: 0xff0000})
+						);
+		this.scene.add(mesh);
+
+
+
+
+
+ 		var light = new THREE.PointLight( 0xFFFF00 );
+        light.position.set( 20, 20, 60 );
+		this.scene.add(light);
+
+	
+		this.camera.lookAt(mesh.position);
+
+	},
+	// render stuff
+	render: function() {
+		//this.camera.rotation.x += 0.2;
+
+		this.renderer.render(this.scene, this.camera);
+	},
+	// animate cycle
+	animate: function() {
+		requestAnimationFrame(ld31.animate);
+		ld31.render();
+	},
+	// event listeners
+	onKeyUp: function(event) {
+
+	},
+	onKeyDown: function(event) {
+
+	},
+	onMouseMove: function(event) {
+
+	}
+
+
+}
+
+ld31.init();
